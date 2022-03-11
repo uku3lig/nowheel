534dd253128b82955d0f6974e09ff4a3a7a10f63
Uku3lig <thelololomail@gmail.com>
2022-03-11 23:14:29 +0100

    initial commit

---
+++ src/main/java/net/uku3lig/nowheel/mixin/PlayerInventoryMixin.java
@@ -0,0 +1,15 @@
+package net.uku3lig.nowheel.mixin;
+
+import net.minecraft.entity.player.PlayerInventory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerInventory.class)
+public class PlayerInventoryMixin {
+    @Inject(at = @At("HEAD"), method = "scrollInHotbar", cancellable = true)
+    private void onHotbarScroll(double amount, CallbackInfo info) {
+        info.cancel();
+    }
+}
