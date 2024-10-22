3a5e9f76165af3d7c91228de78de69f386aa15d6
uku <hi@uku.moe>
2024-10-22 23:19:15 +0200

    feat: update to 1.21.2

--- src/main/java/net/uku3lig/nowheel/mixin/MouseMixin.java
+++ src/main/java/net/uku3lig/nowheel/mixin/MouseMixin.java
@@ -1,16 +1,16 @@
 package net.uku3lig.nowheel.mixin;
 
-import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.client.Mouse;
 import net.uku3lig.nowheel.NoWheel;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(PlayerInventory.class)
-public class PlayerInventoryMixin {
-    @Inject(at = @At("HEAD"), method = "scrollInHotbar", cancellable = true)
-    private void onHotbarScroll(double amount, CallbackInfo ci) {
+@Mixin(Mouse.class)
+public class MouseMixin {
+    @Inject(method = "onMouseScroll", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;setSelectedSlot(I)V"), cancellable = true)
+    public void onHotbarScroll(long window, double horizontal, double vertical, CallbackInfo ci) {
         if (!NoWheel.getManager().getConfig().isEnabled()) ci.cancel();
     }
 }
