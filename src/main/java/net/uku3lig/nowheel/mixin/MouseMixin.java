8a07ceaf14551af4b4b845501d32f9d14597d321
uku <hi@uku.moe>
2025-05-14 17:28:47 +0200

    feat: use mixinextras
    fixes #2
    fixes Wurst-Imperium/WI-Zoom#63

--- src/main/java/net/uku3lig/nowheel/mixin/MouseMixin.java
+++ src/main/java/net/uku3lig/nowheel/mixin/MouseMixin.java
@@ -1,16 +1,16 @@
 package net.uku3lig.nowheel.mixin;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import net.minecraft.client.Mouse;
+import net.minecraft.entity.player.PlayerInventory;
 import net.uku3lig.nowheel.NoWheel;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Mouse.class)
 public class MouseMixin {
-    @Inject(method = "onMouseScroll", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;setSelectedSlot(I)V"), cancellable = true)
-    public void onHotbarScroll(long window, double horizontal, double vertical, CallbackInfo ci) {
-        if (!NoWheel.getManager().getConfig().isEnabled()) ci.cancel();
+    @WrapWithCondition(method = "onMouseScroll", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;setSelectedSlot(I)V"))
+    public boolean onHotbarScroll(PlayerInventory instance, int slot) {
+        return !NoWheel.getManager().getConfig().isEnabled();
     }
 }
