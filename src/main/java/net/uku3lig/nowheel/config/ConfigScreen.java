2d14933a888fe0d3dcc200cf14bed9cc9e17399e
uku <hi@uku.moe>
2025-12-10 11:17:13 +0100

    feat: migrate to mojmap

--- src/main/java/net/uku3lig/nowheel/config/ConfigScreen.java
+++ src/main/java/net/uku3lig/nowheel/config/ConfigScreen.java
@@ -1,6 +1,6 @@
 package net.uku3lig.nowheel.config;
 
-import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screens.Screen;
 import net.uku3lig.nowheel.NoWheel;
 import net.uku3lig.ukulib.config.option.CyclingOption;
 import net.uku3lig.ukulib.config.option.WidgetCreator;
---
+++ src/main/java/net/uku3lig/nowheel/mixin/MixinMouseHandler.java
@@ -0,0 +1,16 @@
+package net.uku3lig.nowheel.mixin;
+
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import net.minecraft.client.MouseHandler;
+import net.minecraft.world.entity.player.Inventory;
+import net.uku3lig.nowheel.NoWheel;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(MouseHandler.class)
+public class MixinMouseHandler {
+    @WrapWithCondition(method = "onScroll", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/player/Inventory;setSelectedSlot(I)V"))
+    public boolean onHotbarScroll(Inventory instance, int slot) {
+        return NoWheel.getManager().getConfig().isEnabled();
+    }
+}
