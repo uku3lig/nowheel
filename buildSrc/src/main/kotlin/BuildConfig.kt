a5bbad3158bc5bbd55b02c219ace27e8adebeb46
uku <hi@uku.moe>
2026-09-15 22:33:37 +0200

    feat: 26.3

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,8 +1,8 @@
 object BuildConfig {
-    const val MINECRAFT_VERSION: String = "26.1"
-    const val FABRIC_LOADER_VERSION: String = "0.18.6"
-    const val NEOFORGE_VERSION: String = "26.1.1.0-beta"
-    const val UKULIB_VERSION: String = "2.0.0+26.1"
+    const val MINECRAFT_VERSION: String = "26.3"
+    const val FABRIC_LOADER_VERSION: String = "0.19.5"
+    const val NEOFORGE_VERSION: String = "26.3.0.0-beta"
+    const val UKULIB_VERSION: String = "2.2.0+26.3"
 
     const val MOD_VERSION: String = "1.4.0"
 
